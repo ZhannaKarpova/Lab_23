84f390bb0e11d667a2e7bb7ae25317788004856a
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-12-30 01:41:20 +0700

    Добавьте файлы проекта.

---
+++ Lab_23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Введите число n:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                FactorialAsync(n);
+                Console.ReadKey();
+            }
+            static void Factorial(int num)
+            {
+                int result = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    result *= i;
+                }
+                Console.WriteLine($"Факториал числа {num} равен {result}");
+            }
+            static async void FactorialAsync(int num)
+            {
+                await Task.Run(() => Factorial(num));
+            }
+        }
+    }
+}
